f825a56d4a01d844eb029736de24cf3c5e13ab73
maxdata <maxdata@maxdatadeMacBook-Pro.local>
2017-10-11 11:50:27 +0800

    index img auto project

--- resources/views/admin/charts/search/s-content.blade.php
+++ resources/views/admin/charts/search/s-content.blade.php
@@ -50,10 +50,10 @@
     .feed-activity-list .feed-element {
         border-bottom: 1px solid #e7eaec;
     }
-    .feed-element, .media-body {
+    .feed-element, .select-body {
         overflow: hidden;
     }
-    .feed-element, .feed-element .media {
+    .feed-element, .feed-element .select {
         margin-top: 15px;
     }
     .feed-element {
@@ -65,7 +65,7 @@
     .pull-left {
         float: left!important;
     }
-    .media-body {
+    .select-body {
         overflow: hidden;
         font-size: 12px;
         display: table-cell;
@@ -107,7 +107,7 @@
                         <div class="feed-activity-list">
 
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right text-navy">1m ago</small>
                                     <strong>Sandra Momot</strong> started following <strong>Monica Smith</strong>. <br>
                                     <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
@@ -119,7 +119,7 @@
                             </div>
 
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right">5m ago</small>
                                     <strong>Monica Smith</strong> posted a new blog. <br>
                                     <small class="text-muted">Today 5:60 pm - 12.06.2014</small>
@@ -128,7 +128,7 @@
                             </div>
 
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right">2h ago</small>
                                     <strong>Mark Johnson</strong> posted message on <strong>Monica Smith</strong> site. <br>
                                     <small class="text-muted">Today 2:10 pm - 12.06.2014</small>
@@ -144,18 +144,18 @@
                                 </div>
                             </div>
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right">2h ago</small>
                                     <strong>Janet Rosowski</strong> add 1 photo on <strong>Monica Smith</strong>. <br>
                                     <small class="text-muted">2 days ago at 8:30am</small>
                                     <div class="photos">
-                                        <a target="_blank" href="http://24.media.tumblr.com/20a9c501846f50c1271210639987000f/tumblr_n4vje69pJm1st5lhmo1_1280.jpg"> <img alt="image" class="feed-photo" src="img/p1.jpg"></a>
-                                        <a target="_blank" href="http://37.media.tumblr.com/9afe602b3e624aff6681b0b51f5a062b/tumblr_n4ef69szs71st5lhmo1_1280.jpg"> <img alt="image" class="feed-photo" src="img/p3.jpg"></a>
+                                        <a target="_blank" href="http://24.select.tumblr.com/20a9c501846f50c1271210639987000f/tumblr_n4vje69pJm1st5lhmo1_1280.jpg"> <img alt="image" class="feed-photo" src="img/p1.jpg"></a>
+                                        <a target="_blank" href="http://37.select.tumblr.com/9afe602b3e624aff6681b0b51f5a062b/tumblr_n4ef69szs71st5lhmo1_1280.jpg"> <img alt="image" class="feed-photo" src="img/p3.jpg"></a>
                                     </div>
                                 </div>
                             </div>
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right text-navy">5h ago</small>
                                     <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
                                     <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
@@ -166,7 +166,7 @@
                                 </div>
                             </div>
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right">2h ago</small>
                                     <strong>Kim Smith</strong> posted message on <strong>Monica Smith</strong> site. <br>
                                     <small class="text-muted">Yesterday 5:20 pm - 12.06.2014</small>
@@ -180,17 +180,14 @@
                                 </div>
                             </div>
                             <div class="feed-element">
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right">23h ago</small>
                                     <strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
                                     <small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
                                 </div>
                             </div>
                             <div class="feed-element">
-                                <a href="#" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/a7.jpg">
-                                </a>
-                                <div class="media-body ">
+                                <div class="select-body ">
                                     <small class="pull-right">46h ago</small>
                                     <strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
                                     <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
