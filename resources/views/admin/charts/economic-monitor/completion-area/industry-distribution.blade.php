83cff4eef5aa03cedffc8f200c15772ceee09562
maxdata <maxdata@maxdatadeMacBook-Pro.local>
2017-10-12 14:01:01 +0800

    area completion title update

--- resources/views/admin/charts/economic-monitor/completion-area/industry-distribution.blade.php
+++ resources/views/admin/charts/economic-monitor/completion-area/industry-distribution.blade.php
@@ -2,7 +2,7 @@
     <div class="col-lg-12">
         <div class="ibox float-e-margins">
             <div class="ibox-title">
-                <h5>行业发展分布</h5>
+                <h5>行业发展分析</h5>
                 <div class="ibox-tools">
                     <a class="collapse-link">
                         <i class="fa fa-chevron-up"></i>
