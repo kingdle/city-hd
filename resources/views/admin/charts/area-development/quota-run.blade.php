147cee147b877bb1317a732e8ec12e3d6ff3be8f
maxdata <maxdata@maxdatadeMacBook-Pro.local>
2017-10-12 12:52:35 +0800

    经济运行对标分析图表数据更新

--- resources/views/admin/charts/area-development/quota-run.blade.php
+++ resources/views/admin/charts/area-development/quota-run.blade.php
@@ -32,7 +32,7 @@
                 trigger: 'axis'
             },
             legend: {
-                data: ['全国', '山东省', '青岛市', '西海岸新区', '浦东新区']
+                data:['西海岸新区','威海市','德州市','浦东新区']
             },
             grid: {
                 left: '3%',
@@ -40,52 +40,46 @@
                 bottom: '3%',
                 containLabel: true
             },
-            toolbox: {},
+            toolbox: {
+
+            },
             xAxis: {
                 type: 'category',
                 boundaryGap: false,
-                data: ['2016-3', '2016-6', '2016-9', '2016-12', '2017-3', '2017-6']
+                data: ['2016-3','2016-6','2016-9','2016-12','2017-3','2017-6']
             },
             yAxis: {
                 type: 'value'
             },
             series: [
                 {
-                    name: '全国',
-                    type: 'line',
-                    stack: '总量',
-                    data: [6.7, 6.7, 6.7, 6.7, 6.9, 6.9]
+                    name:'德州市',
+                    type:'line',
+                    //stack: '总量',
+                    data:[6.9, 6.8, 7, 7.2, 8.5, 8.5]
                 },
                 {
-                    name: '山东省',
-                    type: 'line',
-                    stack: '总量',
-                    data: [7.9, 7.5, 8, 7.5, 7.9, 7]
+                    name:'威海市',
+                    type:'line',
+                    //stack: '总量',
+                    data:[8, 7.8, 7.7, 8, 7.7, 7.7]
                 },
                 {
-                    name: '青岛市',
-                    type: 'line',
-                    stack: '总量',
-                    data: [7.4, 7.3, 7.7, 7.9, 11.3, 7.7]
+                    name:'西海岸新区',
+                    type:'line',
+                    //stack: '总量',
+                    data:[8.5, 10.5, 11.5, 12.3, 10, 12.1]
                 },
                 {
-                    name: '西海岸新区',
-                    type: 'line',
-                    stack: '总量',
-                    data: [8.5, 10.5, 11.5, 12.3, 10, 12.1]
-                },
-                {
-                    name: '浦东新区',
-                    type: 'line',
-                    stack: '总量',
-                    data: [8.3, 7.9, 8.2, 8.1, 7.9, 8]
+                    name:'浦东新区',
+                    type:'line',
+                    //stack: '总量',
+                    data:[8.3, 7.9, 8.2, 8.1, 7.9, 8]
                 }
             ]
         };
 
-
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
-
     });
 </script>
