3a821a3824541775461d093c4d74aad8c30f168a
maxdata <maxdata@maxdatadeMacBook-Pro.local>
2017-10-22 11:43:23 +0800

    area 完成除时间轴外数据关联bug1

--- app/Admin/Controllers/CompletionareaController.php
+++ app/Admin/Controllers/CompletionareaController.php
@@ -28,6 +28,7 @@ class CompletionAreaController extends Controller
             });
             $content->row(function (Row $row) {
                 $row->column(12, view('admin.charts.economic-monitor.completion-area.gross-value'));
+//                $row->column(4, view('admin.charts.economic-monitor.completion-area.table'));
             });
 
         });
